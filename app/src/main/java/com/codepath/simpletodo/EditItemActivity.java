eb3ac908bbda032d0a217d71aca2a0ca19d2ea39
Udit Mehta <umehta@groupon.com>
2015-10-02 01:30:15 -0700

    initial version of TODO APP

---
+++ app/src/main/java/com/codepath/simpletodo/EditItemActivity.java
@@ -0,0 +1,58 @@
+package com.codepath.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    EditText edit;
+    int position;
+    String text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        position = getIntent().getIntExtra("position", 0);
+        text = getIntent().getStringExtra("text");
+        edit = (EditText)findViewById(R.id.editText2);
+        edit.setText(text);
+        edit.setSelection(edit.getText().length());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onEditItem(View v) {
+        Intent editIntent = new Intent();
+        editIntent.putExtra("editText", edit.getText().toString());
+        editIntent.putExtra("position", position);
+        setResult(RESULT_OK, editIntent);
+        finish();
+    }
+
+}
